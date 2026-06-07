beb84b7ab63bc4d48d4adbb85c04372e64b09d69
SvetlanaPismenchuk <spismenchuk3085@floridapoly.edu>
2026-06-07 18:19:55 +0000

    Folder

--- Task_One/Taskone.py
+++ Task_One/Taskone.py
@@ -1,9 +1,9 @@
 # Task One
 
 # Opens Nat_Gas as file and prints the contents
-with open("Nat_Gas.csv", "r", encoding="utf-8") as file:
-    content = file.read()
-    print(content)
+#with open("#", "r", encoding="utf-8") as file:
+ #   content = file.read()
+  #  print(content)
 
 # asks user to input date
 input("Enter the month and year (mm/YYYY)")

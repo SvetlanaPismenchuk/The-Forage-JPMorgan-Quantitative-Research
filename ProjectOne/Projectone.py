b23564a61685fdf31188572f9fd4a5a9084f0427
SvetlanaPismenchuk <spismenchuk3085@floridapoly.edu>
2026-06-07 20:38:09 +0000

    Abstracted Code

---
+++ ProjectOne/Projectone.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+def extract_data("Nat_Gas.csv"):
+    dates = []
+    prices = []
+
+    with open("Nat_Gas.csv", "r") as file:
+        next(file)  # skip header
+
+        for line in file:
+            date_str, price_str = line.strip().split(",")
+
+            # convert date (IMPORTANT FORMAT CHANGE)
+            date = datetime.strptime(date_str, "%m/%d/%y")
+
+            price = float(price_str)  
+
+            dates.append(date)
+            prices.append(price)
+
+    return dates, prices
+
+
+    def convert_to_days(dates):
+        base = dates[0]
+        days = [(d - base).days for d in dates]
+    return days, base
+
+
+    import numpy as np
+
+def fit_model(days, prices):
+    coeffs = np.polyfit(days, prices, 1)  # linear trend
+    return coeffs
